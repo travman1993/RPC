31652f57c437e55e82287688c13e5af557c46a14
Travis Rodriguez <travisgolembiewski@gmail.com>
2023-11-21 21:52:03 -0500

    more fixing

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-const choices = ["rock","paper","scissors"]
+const choices = ["rock", "paper", "scissors"];
 const winner = [];
 
 function game() {
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 1; i <= 5; i++) {
         playRound(i);
     }
+    document.querySelector('button').textContent = 'Play new game';
     logWins();
 }
 
@@ -13,16 +14,16 @@ function playRound(round) {
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection);
     winners.push(winner);
-    logRound(playerSelection, computerSelection, winner)
+    logRound(playerSelection, computerSelection, winner, round);
 }
 
-function playerChoice () {
+function playerChoice() {
     let input = prompt("Type Rock, Paper, or Scissors");
     while (input == null) {
         input = prompt("Type Rock, Paper, or Scissors");
     }
     input = input.toLowerCase();
-    let check = validateInput(input)
+    let check = validateInput(input);
     while (check == false) {
         input = prompt(
             "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
@@ -36,19 +37,17 @@ function playerChoice () {
     return input;
 }
 
-function computerChoice(){
-    return choices[Math.floor(Math.random()*choices.length)]
+function computerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function validateInput(choice) {
-    if (choices.includes(choice)) {
-        return true;
-    } return false;
+    return choices.includes(choice);
 }
 
-function checkWinner(choiceP, choiceC){
-    if(choiceP === choiceC){
-        return 'Tie';
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return "Tie";
     } else if (
         (choiceP === "papper" && choiceC == "rock") || 
         (choiceP === "rock" && choiceC == "scissors") ||
@@ -64,16 +63,16 @@ function logWins() {
     let playerWins = winners.filter((item) => item == "Player").length;
     let computerWins = winners.filter((item) => item == "Computer").length;
     let ties = winners.filter((item) => item == "Tie").length;
-    console.log("Results");
+    console.log("Results:");
     console.log("Player Wins:", playerWins);
-    console.log("Computer Wins", computerWins);
+    console.log("Computer Wins:", computerWins);
     console.log("Ties:", ties);
 }
 
-function logRound(playerChoice,computerChoice,winner,round){
-    console.log('Round',round);
-    console.log('Player Chose:',comuterChoice);
-    console.log('Computer Chose:',computerChoice);
-    console.log(winer, 'Won the Round')
+function logRound(playerChoice, computerChoice, winner, round) {
+    console.log('Round:',round);
+    console.log('Player Chose::', playerChoice);
+    console.log('Computer Chose:', computerChoice);
+    console.log(winner, 'Won the Round:');
     console.log("---------------------------------");
 }
