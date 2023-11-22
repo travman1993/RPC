eb0c11f068e8e328a62a6c4753f6523a2be783c8
Travis Rodriguez <travisgolembiewski@gmail.com>
2023-11-21 20:57:43 -0500

    finnishing up

--- game.js
+++ game.js
@@ -1,12 +1,18 @@
 const choices = ["rock","paper","scissors"]
 
 function game() {
-    playRound()
+    for (let i = 0; i <= 5; i++) {
+        playRound();
+    }
+    logWins();
 }
 
-function playRound() {
+function playRound(round) {
     const playSelection = playerChoice();
     const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection,computerSelection)
+    winners.push(winner);
+    logRound(playerSelection,computeSelection,winner)
 }
 
 function playerChoice () {
@@ -16,10 +22,17 @@ function playerChoice () {
     }
     input = input.toLowerCase();
     let check = validateInput(input)
-    if(check == true){
-        console.log(input);
+    while (check == false) {
+        input = prompt(
+            "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
+            );
+            while (input == null) {
+                input = prompt("Type Rock, Paper, or Scissors");
+            }
+        input = input.toLowerCase();
+        check = validateInput(input);
     }
-    //console.log(input);
+    return input;
 }
 
 function computerChoice(){
@@ -29,9 +42,38 @@ function computerChoice(){
 function validateInput(choice) {
     if (choices.includes(choice)) {
         return true;
+    } return false;
+}
+
+function checkWinner(choiceP, choiceC){
+    if(choiceP === choiceC){
+        return 'Tie';
+    } else if (
+        (choiceP === "papper" && choiceC == "rock") || 
+        (choiceP === "rock" && choiceC == "scissors") ||
+        (choiceP === "scissors" && choiceC == "paper")
+    ) {
+        return "Player";
     } else {
-        return false;
+        return "Computer";
     }
+}
+
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results");
+    console.log("Player Wins:", playerWins);
+    console.log("Computer Wins", computerWins);
+    console.log("Ties:", ties);
+}
+
+function logRound(playerChoice,computerChoice,winner,round){
+    console.log('Round',round);
+    console.log('Player Chose:',comuterChoice);
+    console.log('Computer Chose:',computerChoice);
+    console.log(winer, 'Won the Round')
 }
 
 game();
