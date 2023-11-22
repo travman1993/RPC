573558ecf61c865436bfa5f6baa77c2385426da0
Travis Rodriguez <travisgolembiewski@gmail.com>
2023-11-21 19:48:38 -0500

    .js added returns, function

--- game.js
+++ game.js
@@ -10,9 +10,26 @@ function playRound() {
 }
 
 function playerChoice () {
-    let imput = prompt("Type Rock, Paper, or Scissors");
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if(check == true){
+        console.log(input);
+    }
+    //console.log(input);
 }
 
 function computerChoice(){
     return choices[Math.floor(Math.random()*choices.length)]
+}
+
+function validateInput(choice) {
+    if (choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
 }
